b455744ee99d1fe7d65255292bb672afaf608a1e
finfaterodal <finfanterodal@danielcastelao.org>
2020-03-18 21:09:01 +0100

    Firebase Auteticación implementación lógica email+google.

--- src/app/servicio-de-autent.service.ts
+++ src/app/servicio-de-autent.service.ts
@@ -1,17 +1,23 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {map} from 'rxjs/operators';
+import {auth} from 'firebase';
+import {error} from 'util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioDeAutentService {
 
-  constructor(public auth: AngularFireAuth) {
+  email = '';
+  pass = '';
+  authUser = null;
+
+  constructor(public authApp: AngularFireAuth) {
   }
 
   /*Observable para ver el estado de la sesión del usuario*/
-  user = this.auth.authState.pipe(map(authState => {
+  user = this.authApp.authState.pipe(map(authState => {
     console.log('authState: ', authState);
     if (authState) {
       return authState;
@@ -21,14 +27,29 @@ export class ServicioDeAutentService {
   }));
 
   login() {
-    console.log('login!');
+    console.log('email login!');
+    this.authApp.auth.signInWithEmailAndPassword(this.email, this.pass)
+      .then(user => {
+        console.log('user: ', user);
+      })
+      .catch(error2 => {
+        console.log('error en email login: ', error2);
+      });
   }
 
   glogin() {
     console.log('gogle login!');
+    this.authApp.auth.signInWithPopup(new auth.GoogleAuthProvider())
+      .then(user => {
+        console.log('user: ', user);
+      })
+      .catch(error2 => {
+        console.log('error en google login: ', error2);
+      });
   }
 
   logout() {
     console.log('logout!');
+    this.authApp.auth.signOut();
   }
 }
